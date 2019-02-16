68b892e6f3f15a11375fda51b120d0f1c83f6000
Your Name <you@example.com>
2019-02-16 10:29:46 -0500

    added user specific routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -18,5 +18,19 @@ router.get('/services', indexController.displayServicesPage);
 /* GET contact page. */
 router.get('/contact', indexController.displayContactPage);
 
+/*Get display login page*/
+router.get('/login',indexController.displayLoginPage);
+
+/*POST process login page */
+router.post('/login',indexController.processLoginPage);
+
+/*GET displays the registration page */
+router.get('/register',indexController.displayRegistrationPage);
+
+/* POST process the registration page*/
+router.post('/registration',indexController.processRegistrationPage);
+
+/* GET - perform the logout request*/
+router.get('/logout',indexController.PerformLogout);
 
 module.exports = router;
